981b65a8fd5204907c3bd9e80f0dc401a38c9e8c
Kurontos <kurontos445@gmail.com>
2025-12-11 11:01:33 +0100

    Remove redundant API Platform package

--- config/bundles.php
+++ config/bundles.php
@@ -8,6 +8,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
-    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
